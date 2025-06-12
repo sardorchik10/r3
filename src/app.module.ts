f7adffdeae4c37f5d145b0080b3603d98087f56f
sardorchik10 <sardorbeksafarliyev001@gmail.com>
2025-06-12 17:10:09 +0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
